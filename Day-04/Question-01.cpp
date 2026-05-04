71bc80c802824a1accc124e0d5505c9f356e66df
faizanmd01 <mohammedfaizanmf0786@gmail.com>
2026-05-04 13:47:02 +0530

    first commit

---
+++ Day-04/Question-01.cpp
@@ -0,0 +1,13 @@
+// Pattern
+
+#include <iostream> 
+using namespace std;
+int main() {
+    for (int i = 1; i <= 4; i++) {
+        for (int j = 1; j <= 5; j++) {
+            cout <<"* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
